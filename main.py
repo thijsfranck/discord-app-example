2075e7cf9210e4727546a6f191ad4e896a98e42d
thijsfranck <thijsfranck@gmail.com>
2024-07-06 22:09:12 +0000

    fix: account for missing token

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ from discord_app_example import client
 
 if __name__ == "__main__":
     load_dotenv()
-    client.run(os.getenv("DISCORD_TOKEN"))
+
+    TOKEN = os.getenv("DISCORD_TOKEN")
+
+    if not TOKEN:
+        message = "Discord bot token not found. Please set the DISCORD_TOKEN environment variable."
+        raise ValueError(message)
+
+    client.run(TOKEN)
