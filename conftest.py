f9fe7bf8b7f11196fd3956ce21bcebc5c760227e
thijsfranck <thijsfranck@gmail.com>
2024-07-07 09:21:21 +0000

    test: load environment variables for unit tests

--- conftest.py
+++ conftest.py
@@ -3,3 +3,12 @@ Pytest configuration file.
 
 This file is used to define global fixtures that can be used in all unit tests.
 """
+
+import pytest
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _load_env_vars() -> None:
+    """Load environment variables from .env file."""
+    load_dotenv()
