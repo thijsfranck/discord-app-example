9eb0e73da2d97cd9897720916af64b4d1f8a5bc2
thijsfranck <thijsfranck@gmail.com>
2024-07-07 09:40:05 +0000

    ci: mark project folder safe before installing dependencies

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Install dependencies
-poetry install
-
 # Mark the git repository as safe
 git config --global --add safe.directory $PWD
+
+# Install dependencies
+poetry install
 
 # Install pre-commit hooks
 poetry run pre-commit install
