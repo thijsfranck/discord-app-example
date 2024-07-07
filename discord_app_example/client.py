675797f517891a869333b62a33cd5a9605e86fdb
thijsfranck <thijsfranck@gmail.com>
2024-07-07 11:08:56 +0000

    feat: set up root logger

--- discord_app_example/client.py
+++ discord_app_example/client.py
@@ -1,8 +1,20 @@
+import logging
+
 from discord import Client, Intents
 
 client = Client(intents=Intents.default())
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
+
 
 @client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+async def on_ready() -> None:
+    """Handle the on_ready event."""
+    logger.info("%s has connected to Discord!", client.user)
