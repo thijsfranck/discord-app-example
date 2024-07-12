89a89e21dbc7d756e051248da39565e067317bd5
thijsfranck <thijsfranck@gmail.com>
2024-07-12 18:40:51 +0000

    ci: add lifecycle step and remote cache

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,8 +3,5 @@
 # Mark the git repository as safe
 git config --global --add safe.directory $PWD
 
-# Install dependencies
-poetry install
-
 # Install pre-commit hooks
 poetry run pre-commit install
